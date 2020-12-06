4d16808aa41d22640d8cb85799698ab00b1886ee
he10910 <>
2020-12-06 14:30:07 +0800

    修改的東西都放在readme了

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 import time
 import psutil
+import json
+from os import _exit
 
 options = webdriver.ChromeOptions()
 options.add_argument("--incognito")
 
-username = ('xyz123@xxx.com') #輸入帳號
-password = ('password') #輸入密碼
+with open("config.json" , "r" , encoding="utf-8") as jfile:
+    jdata = json.load(jfile)
 
-driver = webdriver.Chrome(options=options)
+username = (jdata["username"]) #輸入帳號   #在json裡面打
+password = (jdata["password"]) #輸入密碼   #同上
+
+driver = webdriver.Chrome(options=options,executable_path=jdata["PATH"])    #PATH可以在json裡面改
 driver.get('https://my.noip.com/#!/dynamic-dns')
 
 username_textbox = driver.find_element_by_name("username")
@@ -20,8 +25,15 @@ password_textbox.send_keys(password)
 login_button = driver.find_element_by_name("Login")
 login_button.submit()
 
-Confirm_button =driver.find_element_by_css_selector('[class="btn btn-labeled btn-confirm"]')     
 
-Confirm_button.click()
+n = 0
+while n <= 3:
+    try:
+        driver.find_element_by_css_selector('[class="btn btn-labeled btn-confirm"]')
+    except:
+        driver.quit()
+        _exit(0)
 
-driver.quit()
+    Confirm_button =driver.find_element_by_css_selector('[class="btn btn-labeled btn-confirm"]')     
+    Confirm_button.click()
+    n += 1
